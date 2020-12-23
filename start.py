37ae8325e5708683e804a92b67e8c5780cfd6143
zakarouf <zak.d.official@gmail.com>
2020-12-23 19:46:51 +0530

    Updated datafiles, install script

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os, sys, fnmatch
 
-folderIgnore={"data", "doc" ,"stdio", ".git", "SDL", "tisk", "vulkan"}
+folderIgnore={"data", "doc" ,"stdio", ".git", "SDL", "tisk", "vulkan", "opengl"}
 IgnoreNames={"cave_hunter.c"}
 
 
