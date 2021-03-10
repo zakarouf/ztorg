14e64dbf7d8b6573f449da7742827d33c3659e1f
zakarouf <zak.d.official@gmail.com>
2021-03-10 23:06:52 +0530

    updated tisk headers

--- src/zse/io/tisk/tisk.h
+++ src/zse/io/tisk/tisk.h
@@ -1,9 +1,23 @@
 #ifndef ZSE_RENDER_TISK_TERMINAL_H
 #define ZSE_RENDER_TISK_TERMINAL_H
 
+
+#include "../../common.h"
+
 #include "render_tisk_color.h"
 #include "render_tisk_sprite.h"
+#include "render_tisk_io.h"
 
-void zse_r_tiskTest();
+
+#define zse_rtT_getmaxx() 1390
+#define zse_rtT_getmaxy() 800
+
+void zse_rtT_init(void);
+void zse_rtT_exit(void);
+
+void zse_rtT_showCursor(void);
+void zse_rtT_hideCursor(void);
+void zse_rtT__setCur(int x, int y);
+void zse_rtT__set00(void);
 
 #endif
--- src/zse/io/tisk/tisk_intra.h
+++ src/zse/io/tisk/tisk_intra.h
@@ -5,35 +5,4 @@
 #include <stdio.h>
 #include "../../common.h"
 
-frinl static void zse_rtT__setCur(int x, int y)
-{
-	printf("\x1b[%d;%dH", x, y);
-}
-
-frinl static void zse_rtT__set00()
-{
-    write(STDOUT_FILENO, "\x1b[H", sizeof("\x1b[H"));
-}
-
-frinl static void zse_rtT_init()
-{
-    write(STDOUT_FILENO, "\x1b[2J", sizeof("\x1b[2J"));
-}
-
-frinl static void zse_rtT_exit()
-{
-    zse_rtT__set00();
-    write(STDOUT_FILENO, "\x1b[2J", sizeof("\x1b[2J"));
-}
-
-frinl static void zse_rtT_showCursor()
-{
-    write(STDOUT_FILENO, "\x1b[?25h", sizeof("\x1b[?25h"));
-}
-
-frinl static void zse_rtT_hideCursor()
-{
-    write(STDOUT_FILENO, "\x1b[?25l", sizeof("\x1b[?25l"));
-}
-
 #endif
