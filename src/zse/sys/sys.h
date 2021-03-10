d6e48053404c726b222ae0247c73591f9b94353b
zakarouf <zak.d.official@gmail.com>
2021-03-10 23:10:06 +0530

    removed zse_sswitchS, added sleep function to public header

--- src/zse/sys/sys.h
+++ src/zse/sys/sys.h
@@ -9,12 +9,11 @@ int zse_sys_formatCheck(char version[]);
 long zse_sys_getRamUsage(void);
 
 zse_int zse_sswitch(zse_int i);
-zse_int zse_sswitchS(zse_int i);
 
 #define zse_sswitch_M(i) i* -1
-#define zse_sswitchS_M(i) (0b11111111111111111111111111111111 ^ i) + 1
 
 int zse_sys_getLocalTime(int *h, int *m, int *s);
+void zse_sys_msleep(int milliseconds);
 
 #define zse_sys_fillArrayVal(array, val, len)\
     for (int i = 0; i < len; ++i)\
