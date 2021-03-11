21c46f42598226ad6b90d309d7b7b647485ddd38
zakarouf <zak.d.official@gmail.com>
2021-03-11 12:24:41 +0530

    Made progress on vulkan Validation Layer

--- src/zse/io/vulkan/render_vulkan_main.c
+++ src/zse/io/vulkan/render_vulkan_main.c
@@ -1,23 +1,112 @@
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <cglm/cglm.h>
 
-
 #include "../../sys/sys.h"
-
 #include "vulkan.h"
 
-#define NOTPUB_log_normal(format , ...)\
-    zse_sys_log(stdout,"rVK:" format, ##__VA_ARGS__ )
+//#define ZSE_CONFIG__rVK__MINIATURE
+#define ZSE_CONFIG__rVK__NDEBUG
 
-#define NOTPUB_log_error(format , ...)\
-    zse_sys_log(stderr,"rVK:ERROR" format, ##__VA_ARGS__ )
+#ifndef ZSE_CONFIG__rVK__MINIATURE
+    #ifdef ZSE_CONFIG__rVK__NDEBUG
+        const bool enableValidationLayers = false;
+    #else
+        const bool enableValidationLayers = true;
+    #endif
+#endif
 
-static GLFWwindow* GLOBAL_window;
-static VkInstance GLOBAL_vulkan_instance;
+
+#ifdef ZSE_CONFIG_LOGS__USE_VARDIC_MACROS
+    #define NOTPUB_log_normal(format , ...)\
+        zse_sys_log(stdout,"rVK:" format, ##__VA_ARGS__ )
+
+    #define NOTPUB_log_error(format , ...)\
+        zse_sys_log(stderr,"rVK:ERROR:" format, ##__VA_ARGS__ )
+
+#endif
+
+
+
+
+static GLFWwindow* GLOBAL_rVK_window;
+static VkInstance GLOBAL_rVK_vulkan_instance;
+
+static const char * GLOBAL_rVK_validationLayers = "VK_LAYER_KHRONOS_validation";
+static const int GLOBAL_rVK_validationLayersCount = 1;
+
+
+static VkResult _zse_rVK_createDebugUtilsMessengerEXT
+(
+    VkInstance instance
+    , const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo
+    , const VkAllocationCallbacks* pAllocator
+    , VkDebugUtilsMessengerEXT* pDebugMessenger
+) {
+    PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != NULL) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+static void _zse_rVK_DestroyDebugUtilsMessengerEXT
+(
+    VkInstance instance
+    , VkDebugUtilsMessengerEXT debugMessenger
+    , const VkAllocationCallbacks* pAllocator
+) {
+    PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != NULL) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+#ifndef ZSE_CONFIG__rVK__MINIATURE
+static bool checkValidationLayerSupport(const char *validationLayers, const int validationLayersCount) {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, NULL);
+
+    VkLayerProperties *availableLayers = malloc(sizeof(VkLayerProperties) *layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers);
+
+
+    for (int i = 0; i < layerCount; ++i)
+    {
+        NOTPUB_log_normal("%s\n", availableLayers[i].layerName);
+    }
+    free(availableLayers);
+    return 0;
+
+    #define checkValdition(val, aval)\
+    {\
+        bool layerFound = false;\
+        for (int i = 0; i < layerCount; i++) {\
+            if (strcmp(val, aval[i].layerName) == 0) {\
+                    layerFound = true;\
+                    break;\
+            }\
+        }\
+        if (!layerFound) {\
+                free(availableLayers);\
+                return false;\
+        }\
+    }
+    checkValdition(validationLayers, availableLayers);
+    #undef checkValdition
+    
+
+    free(availableLayers);
+
+    
+    return true;
+}
+#endif
 
 static GLFWwindow* _zse_rVK_windowInit(uint32_t x, uint32_t y)
 {
@@ -39,8 +128,33 @@ static int _zse_rVK_destroy(GLFWwindow *window, VkInstance instance)
     return 0;
 }
 
+static StringLines_t _zse_rVK_getRequiredExtentions()
+{
+    StringLines_t extentions;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    extentions = z__StringLines_createEmpty(128, glfwExtensionCount+2);
+
+    for (int i = 0; i < glfwExtensionCount; ++i)
+    {
+        
+    }
+
+}
+
 static VkInstance _zse_rVK_createInstance(int *errorCode)
 {
+
+    #ifndef ZSE_CONFIG__rVK__MINIATURE
+        if (enableValidationLayers && !checkValidationLayerSupport(GLOBAL_rVK_validationLayers, GLOBAL_rVK_validationLayersCount)) {
+            NOTPUB_log_error(" validation layers requested, but not available!");
+        }
+    #endif
+
     VkInstance instance;
 
 	VkApplicationInfo appInfo;
@@ -50,7 +164,7 @@ static VkInstance _zse_rVK_createInstance(int *errorCode)
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "ZSE";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_2;
+    appInfo.apiVersion = VK_API_VERSION_1_0;
 
     VkInstanceCreateInfo createInfo;
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -62,6 +176,7 @@ static VkInstance _zse_rVK_createInstance(int *errorCode)
     const char** glfwExtensions;
 
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
 
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
@@ -97,8 +212,8 @@ static VkInstance _zse_rVK_createInstance(int *errorCode)
 int zse_rVK_initVulkan()
 {
     int errorCode;
-    GLOBAL_window = _zse_rVK_windowInit(1000, 800);
-    GLOBAL_vulkan_instance = _zse_rVK_createInstance(&errorCode);
+    GLOBAL_rVK_window = _zse_rVK_windowInit(1000, 800);
+    GLOBAL_rVK_vulkan_instance = _zse_rVK_createInstance(&errorCode);
 
     return errorCode;
 }
@@ -118,9 +233,9 @@ void zse_render_vulkanInitTEST()
         NOTPUB_log_error("CANT init Vulkan\n");
     };
 
-	_zse_rVK_mainLoop(GLOBAL_window);
+	_zse_rVK_mainLoop(GLOBAL_rVK_window);
 
-	_zse_rVK_destroy(GLOBAL_window, GLOBAL_vulkan_instance);
+	_zse_rVK_destroy(GLOBAL_rVK_window, GLOBAL_rVK_vulkan_instance);
 
 }
 
