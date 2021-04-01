700d25f3276a8add16779d7a00fdf1aae2663ce0
zakarouf <zak.d.official@gmail.com>
2021-04-02 00:01:03 +0530

    Update::Opengl: Started working on buffers

--- src/zse/io/opengl/render_opengl_main.c
+++ src/zse/io/opengl/render_opengl_main.c
@@ -21,12 +21,25 @@
 #define priv_LOG_COLOR(color, format, ...)\
 	zse_sys_log(stdout, "\x1b[38;5;%dm" "rGL:" format "\x1b[0m", color, ##__VA_ARGS__ )
 
+typedef struct {
+	z__u32 VBO;
+	z__u32 VAO;
+	z__u32 EBO;
 
-
+}_zse_rGL_objects;
 
 typedef struct {
 	GLFWwindow *_rGL_window;
+	_zse_rGL_objects _rGL_buffObj;
 }_zse_rGL_HANDLERS;
+
+
+z__float GLOBAL_zse_rGL_vertices [9] = {
+	  -0.5f, -0.5, 0.0f
+	,  0.5f, -0.5, 0.0f
+	,  0.0f,  0.5, 0.0f
+};
+
 
 // NOTE: TO MOVE THIS FUNCTION OR REPLACE IT
 static void _zse_rGL_I_processInput(GLFWwindow *window)
@@ -77,6 +90,64 @@ GLFWwindow * _zse_rGL_windowInit (z__u32 x, z__u32 y)
     return window;
 }
 
+static void zse_rGL_shaderCompileStatus(z__u32 vShader)
+{
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vShader, 512, NULL, infoLog);
+        priv_LOG_ERROR("ERROR:%s\n", infoLog);
+    }
+}
+
+static z__u32 _zse_rGL_getShader
+(
+	  const char * pathVert
+	, const char * pathFrag
+	, z__u32 *vertexShaderID
+	, z__u32 *fragmentShaderID
+)
+{
+	z__String vertShaderSource = z__String_createFromFile((char *)pathVert);
+	z__String fragShaderSource = z__String_createFromFile((char *)pathFrag);
+
+	*vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(*vertexShaderID , 1, (const char * const *)&vertShaderSource.data, NULL);
+    glCompileShader(*vertexShaderID);
+    zse_rGL_shaderCompileStatus(*vertexShaderID );
+
+    *fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(*fragmentShaderID, 1, (const char * const *)&fragShaderSource.data, NULL);
+    glCompileShader(*fragmentShaderID);
+    zse_rGL_shaderCompileStatus(*fragmentShaderID);
+
+	z__String_delete(&vertShaderSource);
+	z__String_delete(&fragShaderSource);
+}
+
+static z__u32 _zse_rGL_createShaderProgram(const char *const pathVert, const char *const pathFrag)
+{
+	z__u32 vertexShaderID, fragmentShaderID;
+	_zse_rGL_getShader(pathVert, pathFrag, &vertexShaderID, &fragmentShaderID);
+
+
+    z__u32 shaderProgramID = glCreateProgram();
+    glAttachShader(shaderProgramID, vertexShaderID);
+    glAttachShader(shaderProgramID, fragmentShaderID);
+    glLinkProgram(shaderProgramID);
+
+    glDeleteShader(vertexShaderID);
+    glDeleteShader(fragmentShaderID);
+
+    return shaderProgramID;
+
+}
+
 static void _zse_rGL_destroy(_zse_rGL_HANDLERS *handle)
 {
 	
@@ -93,6 +164,22 @@ static _zse_rGL_HANDLERS *_zse_rGL_createHandle(void)
 	handle->_rGL_window = _zse_rGL_windowInit(640, 480);
 
 	return handle;
+}
+
+static z__int _zse_rGL__drawTriangle
+(
+	  _zse_rGL_objects* buffObj
+	  , z__float vertices[static 9]
+	  , z__u32 vertices_count
+)
+{
+	glGenBuffers(1, &buffObj->VBO);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, buffObj->VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(z__float) * vertices_count, vertices, GL_STATIC_DRAW);
+
+	return 1;
 }
 
 
