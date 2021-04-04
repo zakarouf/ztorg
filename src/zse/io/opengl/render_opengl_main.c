c40e4432f9477cdcf6bbf591f378f6219c65e02f
zakarouf <zak.d.official@gmail.com>
2021-04-04 11:55:15 +0530

    Update: Now we can do multiple VBOs, VAOs and EBOs at once.

--- src/zse/io/opengl/render_opengl_main.c
+++ src/zse/io/opengl/render_opengl_main.c
@@ -9,61 +9,93 @@
 #endif
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 #include "../../common.h"
+#define z__Arr_getTopMT(arr)        arr.data[arr.lenUsed]
 #include "../../sys/sys.h"
 
 #define priv_LOG_MSG(format, ...)\
-	zse_sys_log(stdout,"\x1b[38;5;3m" "rGL:" format "\x1b[0m", ##__VA_ARGS__ )
+    zse_sys_log(stdout,"\x1b[38;5;3m" "rGL:" format "\x1b[0m", ##__VA_ARGS__ )
 #define priv_LOG_ERROR(format, ...)\
-	zse_sys_log(stdout,"\x1b[38;5;1m" "rGL:" format "\x1b[0m", ##__VA_ARGS__ )
+    zse_sys_log(stdout,"\x1b[38;5;1m" "rGL:" format "\x1b[0m", ##__VA_ARGS__ )
 #define priv_LOG_COLOR(color, format, ...)\
-	zse_sys_log(stdout, "\x1b[38;5;%dm" "rGL:" format "\x1b[0m", color, ##__VA_ARGS__ )
+    zse_sys_log(stdout, "\x1b[38;5;%dm" "rGL:" format "\x1b[0m", color, ##__VA_ARGS__ )
 
 typedef struct {
-	z__u32 VBO;
-	z__u32 VAO;
-	z__u32 EBO;
+    z__u32 VBO;
+    z__u32 VAO;
+    z__u32 EBO;
 
-}_zse_rGL_objects;
+}_zse_rGL_object;
 
 typedef struct {
-	GLFWwindow *_rGL_window;
-	_zse_rGL_objects _rGL_buffObj;
+    z__float* vertices;
+    z__u32* indices;
+
+    z__i32 verticesCount;
+    z__i32 indicesCount;
+}_zse_rGL__Type_vtrs;
+
+typedef z__Arr(_zse_rGL_object) _zse_rGL_objects;
+typedef z__Arr(_zse_rGL__Type_vtrs) _zse_rGL_vtrsTYPE;
+
+typedef struct {
+    GLFWwindow *_rGL_window;
+    _zse_rGL_objects _rGL_buffObj;
+    _zse_rGL_vtrsTYPE _rGL_vtrs;
 }_zse_rGL_HANDLERS;
 
 
-z__float GLOBAL_zse_rGL_vertices [9] = {
-	  -0.5f, -0.5, 0.0f
-	,  0.5f, -0.5, 0.0f
-	,  0.0f,  0.5, 0.0f
+z__float GLOBAL_zse_rGL_vertices [] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+
+    -0.47f, -0.47f, 0.0f,
+     0.5f, -0.47f, 0.0f,
+     0.0f,  0.6f, 0.0f,
+
+    -0.1f, -0.1f, 0.0f,
+     0.1f, -0.1f, 0.0f,
+     0.0f,  0.1f, 0.0f
+
 };
 
+unsigned int GLOBAL_zse_rGL_indices[] = { // note that we start from 0!
+    0, 1, 2, // first triangle
+    3, 4, 5,  // second triangle
+    6, 7, 8  // Third triangle
+};
 
 // NOTE: TO MOVE THIS FUNCTION OR REPLACE IT
 static void _zse_rGL_I_processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
 static void _zse_rGL_framebuffer_size_callback(GLFWwindow* window, z__int width, z__int height)
 {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
 GLFWwindow * _zse_rGL_windowInit (z__u32 x, z__u32 y)
 {
-	// Init GLFW
+    // Init GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	#ifdef __APPLE__
-    	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	#endif
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
 
     //glfwWindowHint(GLFW_DECORATED, GL_FALSE);
     //glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
@@ -71,21 +103,21 @@ GLFWwindow * _zse_rGL_windowInit (z__u32 x, z__u32 y)
     GLFWwindow *window = glfwCreateWindow(x, y, "zse (Opengl)", NULL, NULL);
 
     if (window == NULL)
-	{
-	    priv_LOG_ERROR("Failed to create GLFW window");
-	    glfwTerminate();
-	    return NULL;
-	}
-	glfwMakeContextCurrent(window);
+    {
+        priv_LOG_ERROR("Failed to create GLFW window");
+        glfwTerminate();
+        return NULL;
+    }
+    glfwMakeContextCurrent(window);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-    	priv_LOG_ERROR("Failed to initialize GLAD");
-    	return NULL;
-	}
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        priv_LOG_ERROR("Failed to initialize GLAD");
+        return NULL;
+    }
 
-	glViewport(0, 0, x, y);
-	glfwSetFramebufferSizeCallback(window, _zse_rGL_framebuffer_size_callback);
+    glViewport(0, 0, x, y);
+    glfwSetFramebufferSizeCallback(window, _zse_rGL_framebuffer_size_callback);
 
     return window;
 }
@@ -103,37 +135,39 @@ static void zse_rGL_shaderCompileStatus(z__u32 vShader)
     }
 }
 
-static z__u32 _zse_rGL_getShader
+static void _zse_rGL_getShader
 (
-	  const char * pathVert
-	, const char * pathFrag
-	, z__u32 *vertexShaderID
-	, z__u32 *fragmentShaderID
+      const char * pathVert
+    , const char * pathFrag
+    , z__u32 *vertexShaderID
+    , z__u32 *fragmentShaderID
 )
 {
-	z__String vertShaderSource = z__String_createFromFile((char *)pathVert);
-	z__String fragShaderSource = z__String_createFromFile((char *)pathFrag);
+    z__String vertShaderSource = z__String_createFromFile((char *)pathVert);
+    z__String fragShaderSource = z__String_createFromFile((char *)pathFrag);
+    const char *vSource = vertShaderSource.data;
+    const char *fSource = fragShaderSource.data;
 
-	*vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    *vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
 
-    glShaderSource(*vertexShaderID , 1, (const char * const *)&vertShaderSource.data, NULL);
+    glShaderSource(*vertexShaderID , 1, &vSource, NULL);
     glCompileShader(*vertexShaderID);
     zse_rGL_shaderCompileStatus(*vertexShaderID );
 
     *fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
-    glShaderSource(*fragmentShaderID, 1, (const char * const *)&fragShaderSource.data, NULL);
+    glShaderSource(*fragmentShaderID, 1, &fSource, NULL);
     glCompileShader(*fragmentShaderID);
     zse_rGL_shaderCompileStatus(*fragmentShaderID);
 
-	z__String_delete(&vertShaderSource);
-	z__String_delete(&fragShaderSource);
+    z__String_delete(&vertShaderSource);
+    z__String_delete(&fragShaderSource);
 }
 
 static z__u32 _zse_rGL_createShaderProgram(const char *const pathVert, const char *const pathFrag)
 {
-	z__u32 vertexShaderID, fragmentShaderID;
-	_zse_rGL_getShader(pathVert, pathFrag, &vertexShaderID, &fragmentShaderID);
+    z__u32 vertexShaderID, fragmentShaderID;
+    _zse_rGL_getShader(pathVert, pathFrag, &vertexShaderID, &fragmentShaderID);
 
 
     z__u32 shaderProgramID = glCreateProgram();
@@ -144,72 +178,118 @@ static z__u32 _zse_rGL_createShaderProgram(const char *const pathVert, const cha
     glDeleteShader(vertexShaderID);
     glDeleteShader(fragmentShaderID);
 
-    return shaderProgramID;
 
+    char infoLog[512] = {0};
+    z__int success;
+    glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgramID, 512, NULL, infoLog);
+        priv_LOG_ERROR("ERROR:%s\n", infoLog);
+    }
+
+    return shaderProgramID;
+}
+
+static void _zse_rGL__bindObjects
+(
+      _zse_rGL_object* buffObj
+      , z__float vertices[static 1]
+      , z__u32 vertices_count
+)
+{
+    glGenBuffers(1, &buffObj->VBO);
+    glGenVertexArrays(1, &buffObj->VAO);
+
+    glGenBuffers(1, &buffObj->EBO);
+
+    glBindVertexArray(buffObj->VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffObj->VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(z__float) * vertices_count, vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffObj->EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLOBAL_zse_rGL_indices), GLOBAL_zse_rGL_indices, GL_STATIC_DRAW);
+             
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    return;
+}
+
+static void _zse_rGL_deleteShaders(z__u32 *shaders, z__u32 count)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        glDeleteShader(shaders[i]);
+    }
 }
 
 static void _zse_rGL_destroy(_zse_rGL_HANDLERS *handle)
 {
-	
-	glfwDestroyWindow(handle->_rGL_window);
-	glfwTerminate();
-	// At End
-	free(handle);
+    z__Arr_delete(&handle->_rGL_buffObj);
+
+    glfwDestroyWindow(handle->_rGL_window);
+    glfwTerminate();
+    // At End
+    free(handle);
 }
+
 
 static _zse_rGL_HANDLERS *_zse_rGL_createHandle(void)
 {
-	_zse_rGL_HANDLERS *handle = z__MALLOC(sizeof(_zse_rGL_HANDLERS));
+    _zse_rGL_HANDLERS *handle = z__MALLOC(sizeof(_zse_rGL_HANDLERS));
 
-	handle->_rGL_window = _zse_rGL_windowInit(640, 480);
+    z__Arr_create(&handle->_rGL_buffObj, 8);
 
-	return handle;
-}
+    handle->_rGL_window = _zse_rGL_windowInit(640, 480);
 
-static z__int _zse_rGL__drawTriangle
-(
-	  _zse_rGL_objects* buffObj
-	  , z__float vertices[static 9]
-	  , z__u32 vertices_count
-)
-{
-	glGenBuffers(1, &buffObj->VBO);
-
-
-	glBindBuffer(GL_ARRAY_BUFFER, buffObj->VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(z__float) * vertices_count, vertices, GL_STATIC_DRAW);
-
-	return 1;
+    return handle;
 }
 
 
 void zse_rGL_mainloop(_zse_rGL_HANDLERS *Handle)
 {
-	while(!glfwWindowShouldClose(Handle->_rGL_window))
-	{
-		// Input
-		_zse_rGL_I_processInput(Handle->_rGL_window);
+    z__u32 shaderProgramID_1 = _zse_rGL_createShaderProgram("shaders/src/glT.vert", "shaders/src/glT.frag");
+    z__u32 shaderProgramID_2 = _zse_rGL_createShaderProgram("shaders/src/glT.vert", "shaders/src/glT2.frag");
 
-		// Output/Rendering
-		glClearColor(0.9f, 0.5f, 0.7f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+    _zse_rGL__bindObjects(&Handle->_rGL_buffObj.data[0], GLOBAL_zse_rGL_vertices, sizeof(GLOBAL_zse_rGL_vertices)/sizeof(z__float));
 
-		// Update
-	    glfwSwapBuffers(Handle->_rGL_window);
-	    glfwPollEvents();    
-	}
+
+    while(!glfwWindowShouldClose(Handle->_rGL_window))
+    {
+        // Input
+        _zse_rGL_I_processInput(Handle->_rGL_window);
+
+        // Output/Rendering
+        glClearColor(0.04f, 0.02f, 0.01f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgramID_1);
+        glBindVertexArray(Handle->_rGL_buffObj.data[0].VAO);
+        glDrawElements(GL_TRIANGLES, sizeof(GLOBAL_zse_rGL_indices)/sizeof(z__float), GL_UNSIGNED_INT, 0);
+
+        // Update
+        glfwSwapBuffers(Handle->_rGL_window);
+        glfwPollEvents();    
+    }
+
+    glDeleteVertexArrays(1, &Handle->_rGL_buffObj.data[0].VAO);
+    glDeleteBuffers(1, &Handle->_rGL_buffObj.data[0].VBO);
+    glDeleteBuffers(1, &Handle->_rGL_buffObj.data[0].EBO);
+    _zse_rGL_deleteShaders((z__u32[]){shaderProgramID_1, shaderProgramID_2}, 2);
 }
 
 void zse_rGL_exit(_zse_rGL_HANDLERS *rglHandle)
 {
-	_zse_rGL_destroy(rglHandle);
+    _zse_rGL_destroy(rglHandle);
 }
 
 void zse_rGL_init(void)
 {
-	_zse_rGL_HANDLERS *rglHandle = _zse_rGL_createHandle();
-	zse_rGL_mainloop(rglHandle);
-	zse_rGL_exit(rglHandle);
+    _zse_rGL_HANDLERS *rglHandle = _zse_rGL_createHandle();
+    zse_rGL_mainloop(rglHandle);
+    zse_rGL_exit(rglHandle);
 }
 
 
