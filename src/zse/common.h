f2784baf9b5e0d471074ce4dba582501a7605404
zakarouf <zak.d.official@gmail.com>
2021-04-06 12:12:01 +0530

    Added: lib signature

--- src/zse/common.h
+++ src/zse/common.h
@@ -4,7 +4,23 @@
 #include "config.h"
 #include "common/common.h"
 
-#define zse_xyz3Dto1D(x, y, z, X, Y) x + ((y)*X) + ((z)*X*Y)
+#if defined(_MSC_VER)
+#  ifdef CGLM_STATIC
+#    define ZSE__EXPORT
+#  elif defined(CGLM_EXPORTS)
+#    define ZSE__EXPORT __declspec(dllexport)
+#  else
+#    define ZSE__EXPORT __declspec(dllimport)
+#  endif
+#  define ZSE__INLINE __forceinline
+#else
+#  define ZSE__EXPORT __attribute__((visibility("default")))
+#  define ZSE__INLINE static inline __attribute((always_inline))
+#  define ZSE__INLINE_NST inline __attribute((always_inline))
+#endif
+
+
+#define zse_xyz3Dto1D(x, y, z, X, Y) ((x) + ((y)*(X)) + ((z)*(X)*(Y)))
 
 typedef struct _Standart_brush_t
 {
