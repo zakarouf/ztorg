2783e3e368df426c44b21ff05313c1f66627c4da
zakarouf <zak.d.official@gmail.com>
2021-04-09 23:42:31 +0530

    Update: Include Changes to get default sprite header

--- src/zse/io/tisk/render_tisk_sprite.h
+++ src/zse/io/tisk/render_tisk_sprite.h
@@ -1,7 +1,7 @@
 #ifndef ZSE_RENDER_STDIO_SPRITES_H
 #define ZSE_RENDER_STDIO_SPRITES_H
 
-#include "../../sprite_new/sprite_char_def.h"
+#include "../../sprite/sprite_char_def.h"
 void zse_rtT__spriteChar__print(const zset__SpriteChar *spr, int frame);
 
 #endif
