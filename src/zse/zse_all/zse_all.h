196d5883fea1a084a50ea96ba37be77c040c7ead
zakarouf <zak.d.official@gmail.com>
2021-04-09 23:16:40 +0530

    Removed: Ncurses raycast render world.

--- src/zse/zse_all/zse_all.h
+++ src/zse/zse_all/zse_all.h
@@ -1,10 +1,7 @@
 #ifndef ZSE_ALL_H
 #define ZSE_ALL_H
 
-
 #include "../zse.h"
 #include "../io/curses/curses.h"
-
-void zse_r_render_world_raycast (WINDOW *win, ENTT_t *player, ST_WORLD_t *map, TILE_t *tile);
 
 #endif
