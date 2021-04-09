184b7f73198c8b3fdd15ff844317b3b7a3c8987a
zakarouf <zak.d.official@gmail.com>
2021-04-09 20:30:09 +0530

    Update: render: tisk: sprite: Adapted print funtion for new Sprite type

--- src/zse/io/tisk/render_tisk_sprite.c
+++ src/zse/io/tisk/render_tisk_sprite.c
@@ -1,19 +1,18 @@
 #include "../../common.h"
-#include "../../sprite/sprite_lib.h"
+#include "../../sprite_new/sprite_char_def.h"
 
 #include "render_tisk_color.h"
 #include <stdio.h>
 
-void zse_rtT_sprite_print(SPRITES_t *spr, int frame)
+void zse_rtT__spriteChar__print(const zset__SpriteChar *spr, int frame)
 {
-	for (int i = 0; i < spr->Y; ++i)
+	for (int i = 0; i < spr->x; ++i)
 	{
-		for (int j = 0; j < spr->X; ++j)
+		for (int j = 0; j < spr->y; ++j)
 		{
-			zse_rtT__color256Fg_set(spr->plot[zse_xyz3Dto1D(i, j, frame, spr->X, spr->Y)]>>8);
-			putc(spr->plot[zse_xyz3Dto1D(i, j, frame, spr->X, spr->Y)]&0xFF, stdout);
+			zse_rtT__color256Fg_set(spr->colormap[zse_xyz3Dto1D(i, j, frame, spr->x, spr->y)]);
+			putc(spr->plot[zse_xyz3Dto1D(i, j, frame, spr->x, spr->y)]&0xff, stdout);
 		}
 		putc('\n', stdout);
 	}
-
 }
