5dae588ad5b1a6c5de9570cb1e93273cf033c671
zakarouf <zak.d.official@gmail.com>
2021-04-12 00:54:25 +0530

    Changed: io: Curses: main header no longer gives access to spr & map draw funtions, we have to include the headers manually.

--- src/zse/io/curses/curses.h
+++ src/zse/io/curses/curses.h
@@ -3,7 +3,7 @@
 
 
 #include <ncurses/ncurses.h>
-#include "render_curses_color.h"
+#include "curses_color.h"
 
 int zse_rtC_init();
 void zse_rtC_exit();
@@ -13,9 +13,5 @@ int zse_rtC_getint();
 int zse_rtC_getint_printw_option(char str[]);
 int zse_rtC_selectListS(WINDOW *win, int x, int y, char **list, int listsize, char *getname);
 void zse_rtC_clearPortion(WINDOW *win, int x, int y, int uptoX, int uptoY);
-
-
-#include "render_curses_map.h"
-#include "render_curses_sprite.h"
 
 #endif
