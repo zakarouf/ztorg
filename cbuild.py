72626ee5297cae43b5c74348dcfaf11e5318bb18
zakarouf <zak.d.official@gmail.com>
2021-04-15 21:45:52 +0530

    Update: Cbuild to use ncursesw instead ncurses.

--- cbuild.py
+++ cbuild.py
@@ -20,7 +20,7 @@ CC="gcc"
 ERRFLAGS = ["-Wall"]
 CFLAGS=["-std=c99", "-ffunction-sections", "-fdata-sections", "-Os", "-O2"] + ERRFLAGS
 #LDFLAGS=["-lm", "-lncurses", "-lraylib"]
-LDFLAGS=["-lm", "-lvulkan", "-lglfw", "-lncurses", "-framework","opengl", "-Wl,-rpath", "-Wl,/usr/local/lib"]
+LDFLAGS=["-lm", "-lvulkan", "-lglfw", "-lncursesw", "-framework","opengl", "-Wl,-rpath", "-Wl,/usr/local/lib"]
 OUTEXE="build/z"
 
 
