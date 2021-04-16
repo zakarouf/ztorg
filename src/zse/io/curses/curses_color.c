06d6bc114460af13ab303b6913972d67d97e2a6d
zakarouf <zak.d.official@gmail.com>
2021-04-16 10:20:19 +0530

    Update: io: Curses: Init color pair to init 32k color pair.

--- src/zse/io/curses/curses_color.c
+++ src/zse/io/curses/curses_color.c
@@ -4,11 +4,48 @@
 int zse_rtC_color_initpairs(void)
 {
 
-	// Colors Text w/ Black b/g from 0 to 256
+	short pair = 0;
+
 	for (int i = 0; i < COLORS; ++i)
 	{
-		init_pair(i, i, -1);
+		init_pair(pair, i, -1);
+		pair++;
 	}
+	// Colors Text w/ b/g from 1 to 256
+	for (int i = 1; i < 16; i += 1) // Bg
+	{
+		for (int j = 0; j < COLORS; ++j)
+		{
+			init_pair(pair, j, i);
+			pair++;
+		}
+	}
+
+#if 0 // Checks
+	short p = 258;
+	init_pair(p, 5, 1);
+
+	wcolor_set(stdscr, (0x4300) | (0x43) , NULL);
+	addstr("adawd");
+	wcolor_set(stdscr, 578 , NULL);
+	addstr("adawdadsa\n");
+
+	wcolor_set(stdscr, 7777 , NULL);
+	addstr("adawdadsa\n");
+
+	
+	wcolor_set(stdscr, p, NULL);
+	addstr("Abc\n");
+
+
+	wcolor_set(stdscr, 1, NULL);
+	addstr("mss\n");
+
+
+	refresh();
+	getch();
+
+#endif
 
 	return 0;
 }
