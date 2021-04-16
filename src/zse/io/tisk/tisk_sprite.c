87aa2502c8a9a48fd1eab4a5ba09cc2cdb9894a6
zakarouf <zak.d.official@gmail.com>
2021-04-16 10:15:06 +0530

    Update: io: tisk: sChar: To use the updated sChar type

--- src/zse/io/tisk/tisk_sprite.c
+++ src/zse/io/tisk/tisk_sprite.c
@@ -6,12 +6,15 @@
 
 void zse_rtT__spriteChar__print(const zset__SpriteChar *spr, int frame)
 {
+	zset__spritechtype *plot = &spr->plot[zse_xyz3Dto1D(0, 0, frame, spr->x, spr->y)];
+	z__u16 *color = &spr->colormap[zse_xyz3Dto1D(0, 0, frame, spr->x, spr->y)];
 	for (int i = 0; i < spr->x; ++i)
 	{
-		for (int j = 0; j < spr->y; ++j)
+		for (int j = 0; j < spr->y; ++j, color++,plot++)
 		{
-			zse_rtT__color256Fg_set(spr->colormap[zse_xyz3Dto1D(i, j, frame, spr->x, spr->y)]);
-			putc(spr->plot[zse_xyz3Dto1D(i, j, frame, spr->x, spr->y)]&0xff, stdout);
+			zse_rtT__color256Fg_set(*color&0xff);
+			zse_rtT__color256Bg_set(*color >> 8);
+			putc(*plot, stdout);
 		}
 		putc('\n', stdout);
 	}
